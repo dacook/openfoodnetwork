c0990b9ebbb7561c3f7b7ab6c3c256cd4d4d6771
David Cook <david@redcliffs.net>
2024-05-14 10:39:14 +1000

    Ignore disconnected form elements
    When elements are removed from the DOM, they remain in the recordElements array. But we can simply ignore them.
    We have to wait until after rails-nested-form:remove is completed before toggleFormChanged.
    
    hmm It would be even better to remove them from the array..

--- app/webpacker/controllers/bulk_form_controller.js
+++ app/webpacker/controllers/bulk_form_controller.js
@@ -91,35 +91,6 @@ export default class BulkFormController extends Controller {
     }
   }
 
-  // Removes tr from DOM as well as corresponding elmnts from recordElement
-  // when clicking on 'Remove' button. For not persisted rows removed by
-  // stimulus components lib.
-  remove(event) {
-    // We need the temp id for the newly non persisted variant
-    let elmnt = event.target;
-    const recordContainer = elmnt.closest("[data-record-id]");
-    let recordId = recordContainer.dataset.recordId;
-    let elmnt_temp_id = elmnt.dataset.temp_id;
-    const re = /([0-9]*)_remove_link/;
-    let temp_id = elmnt_temp_id.match(re)[1]
-
-    // Store indexes and not elements
-    let elmntsToDelete = [];
-    this.recordElements[recordId].forEach((item, index) => {
-      if ((item.id).includes(temp_id)) {
-        elmntsToDelete.push(index);
-      }
-    });
-
-
-    // Want to delete ary.last - ary.first ie from to first up to last index
-    // In this way, we also delete unnecessary elements without reference to temp variant(id)
-    // like buttons but nonetheless part of the elements to be removed.
-    this.recordElements[recordId]
-      .splice(elmntsToDelete[0], elmntsToDelete[elmntsToDelete.length - 1] - elmntsToDelete[0] + 1);
-    this.toggleFormChanged();
-  }
-
   // private
 
   #registerSubmit() {
@@ -161,13 +132,18 @@ export default class BulkFormController extends Controller {
   }
 
   #isChanged(element) {
-    if (element.type == "checkbox") {
+    if(!element.isConnected) {
+      return false;
+
+    } else if (element.type == "checkbox") {
       return element.defaultChecked !== undefined && element.checked != element.defaultChecked;
+
     } else if (element.type == "select-one") {
       const defaultSelected = Array.from(element.options).find((opt) =>
         opt.hasAttribute("selected"),
       );
       return element.selectedOptions[0] != defaultSelected;
+
     } else {
       return element.defaultValue !== undefined && element.value != element.defaultValue;
     }
