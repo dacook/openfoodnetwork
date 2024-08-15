707f987c9a8232dc16a7e565b2c9597479ad0d00
David Cook <david@redcliffs.net>
2024-08-15 11:41:54 +1000

    Revert "[wip] join query to avoid loading mega list of enterprise IDs"
    This reverts commit a4cd65fcd8d0ed06c914cbafde3b46c261a0816b.

--- engines/order_management/app/services/order_management/subscriptions/variants_list.rb
+++ engines/order_management/app/services/order_management/subscriptions/variants_list.rb
@@ -8,29 +8,7 @@ module OrderManagement
       # - Variants of hub
       # - Variants that are in outgoing exchanges where the hub is receiver
       def self.eligible_variants(distributor)
-        # query = Spree::Variant.where(supplier: distributor).union(
-        #   Spree::Variant.joins(supplier: :relationships_as_parent) #.merge(permitted_producer_scope(distributor))
-        #                         .left_outer_joins(supplier: :relationships_as_parent)
-        #                         .merge(EnterpriseRelationship.permitting(distributor.id).with_permission(:add_to_order_cycle))
-        # )
-        #
-        query = Spree::Variant.joins( <<~SQL.squish
-          INNER JOIN enterprises AS supplier
-            ON spree_variants.supplier_id = supplier.id
-          LEFT OUTER JOIN enterprise_relationships
-            ON enterprise_relationships.parent_id = supplier.id
-          INNER JOIN enterprise_relationship_permissions
-            ON enterprise_relationship_permissions.enterprise_relationship_id = enterprise_relationships.id
-          INNER JOIN enterprises AS permitted_enterprise
-            ON enterprise_relationships.child_id = permitted_enterprise.id
-        SQL
-        ).where("
-          spree_variants.deleted_at IS NULL
-          AND (supplier_id = ?
-           OR (enterprise_relationships.child_id = ?
-             AND enterprise_relationship_permissions.name = 'add_to_order_cycle')
-          )
-        ", distributor.id, distributor.id)
+        query = Spree::Variant.where(supplier_id: permitted_producer_ids(distributor))
 
         exchange_variant_ids = outgoing_exchange_variant_ids(distributor)
         if exchange_variant_ids.present?
@@ -49,13 +27,12 @@ module OrderManagement
       end
 
       def self.permitted_producer_ids(distributor)
-        self.permitted_producer_scope(distributor).pluck(:parent_id) | [distributor.id]
-      end
-
-      def self.permitted_producer_scope(distributor)
-        EnterpriseRelationship.joins(:parent)
+        other_permitted_producer_ids = EnterpriseRelationship.joins(:parent)
           .permitting(distributor.id).with_permission(:add_to_order_cycle)
           .merge(Enterprise.is_primary_producer)
+          .pluck(:parent_id)
+
+        other_permitted_producer_ids | [distributor.id]
       end
 
       def self.outgoing_exchange_variant_ids(distributor)
